8da3968d365ad17be4e873d74563d93657ec4b94
Irshad Ali <irshadsheikh2005@gmail.com>
2025-01-03 23:49:18 +0530

    add controllers for comments, dashboard, healthcheck, likes, playlists, subscriptions, tweets, and videos

--- backend/src/controllers/healthcheck.controller.js
+++ backend/src/controllers/healthcheck.controller.js
@@ -1,5 +1,9 @@
-const healthcheck = () => {
-    console.log("healthcheck controller");
-};
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-export default healthcheck;
+const healthcheck = asyncHandler(async (req, res) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+});
+
+export { healthcheck };
